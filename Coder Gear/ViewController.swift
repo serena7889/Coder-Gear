b2586c9d29d71479c182d33b6c1fbd9db0fc54b5
Serena <serenalambert1731@gmail.com>
2019-11-10 15:08:09 +0000

    Initial commit

---
+++ Coder Gear/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Coder Gear
+//
+//  Created by Serena Lambert on 10/11/2019.
+//  Copyright © 2019 Serena Lambert. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
